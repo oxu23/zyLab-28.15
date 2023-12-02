db113effce0efb8869fc77ce8ae29cf127b38f83
oxu23 <139178932+oxu23@users.noreply.github.com>
2023-12-02 13:07:27 -0600

    Update playlistnode.h

--- playlistnode.h
+++ playlistnode.h
@@ -1,1 +1,29 @@
+#ifndef PLAYLISTNODE_H
+#define PLAYLISTNODE_H
+
+#include <string>
+using namespace std;
+
+class PlaylistNode {
+private:
+    string uniqueID;
+    string songName;
+    string artistName;
+    int songLength;
+    PlaylistNode* nextNodePtr;
+
+public:
+    PlaylistNode();
+    PlaylistNode(string id, string songName, string artistName, int songLength);
+    string GetID() const;
+    string GetSongName() const;
+    string GetArtistName() const;
+    int GetSongLength() const;
+    PlaylistNode* GetNext() const;
+    void InsertAfter(PlaylistNode* nodePtr);
+    void SetNext(PlaylistNode* nodePtr);
+    void PrintPlaylistNode() const;
+};
+
+#endif
 
